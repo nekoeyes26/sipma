d6d678cbb9a95fd5dc58717e02ddff3e0e5fa7c8
unknown <ardhillaeka83@gmail.com>
2024-01-10 07:03:46 +0700

    fix part 2

--- resources/views/pimpinan/navbar.blade.php
+++ resources/views/pimpinan/navbar.blade.php
@@ -1,8 +1,7 @@
 <!-- Navbar Start -->
-{{-- <div class="container-fluid bg-primary"> --}}
 <div class="container bg-primary">
     <nav class="navbar navbar-dark navbar-expand-lg py-0 bg-primary">
-        <a href="{{ route('home') }}" class="navbar-brand">
+        <a href="{{ route('pimpinan.aduan') }}" class="navbar-brand">
             <h1 class="text-white fw-bold d-block">SIP<span class="text-secondary">MA</span> </h1>
         </a>
         <button type="button" class="navbar-toggler me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
@@ -10,29 +9,23 @@
         </button>
         <div class="collapse navbar-collapse bg-transparent" id="navbarCollapse">
             <div class="navbar-nav ms-auto mx-xl-auto p-0">
-                <a href="{{ route('pimpinan.aduan') }}" class="nav-item nav-link active text-secondary">Beranda</a>
-                {{-- <a href="about.html" class="nav-item nav-link">Tata Cara</a> --}}
-                {{-- <a href="contact.html" class="nav-item nav-link">Pengaduan</a> --}}
-                <a href="{{ route('pimpinan.tentang') }}" class="nav-item nav-link">Tentang</a>
-                {{-- @if (Auth::guard('guard1')->check() && Auth::guard('guard1')->user()->nip_bakpk)
-                        <a href="#" class="nav-item nav-link">{{ Auth::guard('guard1')->user()->nip_bakpk }}</a>
-                    @endif --}}
+                <a href="{{ route('pimpinan.aduan') }}" class="nav-item nav-link">Beranda</a>
+                <a href="{{ route('pimpinan.tentang') }}" class="nav-item nav-link {{ Request::is('pimpinan/tentang') ? 'active' : '' }}">Tentang</a>
                 <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">List Aduan</a>
+                    <a href="#" class="nav-link dropdown-toggle {{ Request::is('pimpinan/aduan*') ? 'active' : '' }}" data-bs-toggle="dropdown">List Aduan</a>
                     <div class="dropdown-menu rounded">
-                        <a href="{{ route('pimpinan.aduan') }}" class="dropdown-item">Aduan Diterima</a>
-                        <a href="{{ route('pimpinan.aduan.level1') }}" class="dropdown-item">Aduan Level 1</a>
-                        <a href="{{ route('pimpinan.aduan.level2') }}" class="dropdown-item">Aduan Level 2</a>
-                        <a href="{{ route('pimpinan.aduan.level3') }}" class="dropdown-item">Aduan Level 3</a>
-                        <a href="{{ route('pimpinan.aduan.selesai') }}" class="dropdown-item">Aduan Selesai</a>
+                        <a href="{{ route('pimpinan.aduan') }}" class="dropdown-item {{ Request::is('pimpinan/aduan') ? 'active' : '' }}">Aduan Diterima</a>
+                        <a href="{{ route('pimpinan.aduan.level1') }}" class="dropdown-item {{ Request::is('pimpinan/aduan/level1') ? 'active' : '' }}">Aduan Level 1</a>
+                        <a href="{{ route('pimpinan.aduan.level2') }}" class="dropdown-item {{ Request::is('pimpinan/aduan/level2') ? 'active' : '' }}">Aduan Level 2</a>
+                        <a href="{{ route('pimpinan.aduan.level3') }}" class="dropdown-item {{ Request::is('pimpinan/aduan/level3') ? 'active' : '' }}">Aduan Level 3</a>
+                        <a href="{{ route('pimpinan.aduan.selesai') }}" class="dropdown-item {{ Request::is('pimpinan/aduan/selesai') ? 'active' : '' }}">Aduan Selesai</a>
                     </div>
                 </div>
                 <div class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Akun</a>
                     <div class="dropdown-menu rounded">
                         @if (Auth::guard('guard3')->check() && Auth::guard('guard3')->user()->nip_pimpinan)
-                            <a href="#"
-                                class="dropdown-item">{{ Auth::guard('guard3')->user()->nip_pimpinan }}</a>
+                            <a href="#" class="dropdown-item">{{ Auth::guard('guard3')->user()->nip_pimpinan }}</a>
                         @endif
                         @if (Auth::guard('guard1')->check() || Auth::guard('guard2')->check() || Auth::guard('guard3')->check())
                             <form action="/pimpinan/logout" method="POST">
@@ -49,5 +42,4 @@
         </div>
     </nav>
 </div>
-{{-- </div> --}}
 <!-- Navbar End -->
